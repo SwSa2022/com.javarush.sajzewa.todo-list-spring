5de06a9e8b60ad80a7614024549cf9020629a6fd
Swetlana Sajzewa <ssajzewa@wbsedu.de>
2024-10-10 20:52:49 +0200

    second commit

---
+++ src/main/java/com/javarush/config/WebAppInitializer.java
@@ -0,0 +1,23 @@
+package com.javarush.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer  {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return  new Class[0];
+        //return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+        //return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
